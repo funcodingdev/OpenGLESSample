e7141c9f074c6eeb1eee46b99a44e80ab0efe2e5
fangju <funcodingdev@foxmail.com>
2022-02-23 17:52:34 +0800

    feat: 增加主菜单

---
+++ app/src/main/java/cn/funcoding/opengles/sample/google/GoogleSampleActivity.java
@@ -0,0 +1,11 @@
+package cn.funcoding.opengles.sample.google;
+
+import cn.funcoding.opengles.sample.internal.BaseSampleActivity;
+
+/**
+ * 官方例子
+ * https://developer.android.google.cn/training/graphics/opengl
+ */
+public class GoogleSampleActivity extends BaseSampleActivity {
+
+}
---
+++ app/src/main/java/cn/funcoding/opengles/sample/internal/BaseSampleActivity.java
@@ -0,0 +1,38 @@
+package cn.funcoding.opengles.sample.internal;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+
+import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseSampleActivity extends AppCompatActivity {
+
+    @CallSuper
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String title = getIntent().getStringExtra("title");
+        if (!TextUtils.isEmpty(title)) {
+            setTitle(title);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
